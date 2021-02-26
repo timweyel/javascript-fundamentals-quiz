ae04ac3abfcbda7a329d4ad655320632e9b504fe
Tim Weyel <tweyel@gmail.com>
2021-02-25 21:19:34 -0800

    fresh start not using loop for questions. added question text and choice buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,52 @@
 var questionsArray = [{
   question: "Which method removes the first element from an array and returns that removed element? This method changes the length of the array.",
-  choices: ["shift()", "unshift()", "push()", "filter()"],
-  answer: "shift()"
+  choices: ["shift( )", "unshift( )", "push( )", "filter( )"],
+  answer: "shift( )"
 },
 {
   question: "Which method removes the last element from an array and returns that element? This method changes the length of the array.",
-  choices: ["join()", "slice()", "splice()", "pop()"],
-  answer: "pop()"
+  choices: ["join( )", "slice( )", "splice( )", "pop( )"],
+  answer: "pop( )"
 },
 {
   question: "Which method returns a string representing the specified array and its elements?",
-  choices: ["concat()", "fill()", "toString()", "flat()"],
-  answer: "toString"
+  choices: ["concat( )", "fill( )", "toString( )", "flat( )"],
+  answer: "toString( )"
 },
 {
   question: "Which method returns true or false if every element in the specified array satisfies a condition specified in the callback function. Returns false even if single element does not satisfy the condition?",
-  choices: ["forEach()", "reduce()", "map()", "every()"],
-  answer: "every()"
+  choices: ["forEach( )", "reduce( )", "map( )", "every( )"],
+  answer: "every( )"
 },
 {
   question: "Which method passes two elements simultaneously in the callback function (till it reaches the last element) and returns a single value?",
-  choices: ["reduce()", "some()", "indexOf()", "filter()"],
-  answer: "reduce()"
+  choices: ["reduce( )", "some( )", "indexOf( )", "filter( )"],
+  answer: "reduce( )"
 },
 {
   question: "Which method returns a new array with specified start to end elements?",
-  choices: ["slice()", "reverse()", "some()", "map()"],
-  answer: "slice()"
+  choices: ["slice( )", "reverse( )", "some( )", "map( )"],
+  answer: "slice( )"
 },
 {
   question: "Which method adds one or more elements to the front of an array and returns the new length of the array.",
-  choices: ["splice()", "unshift()", "push()", "pop()"],
-  answer: "unshift()"
+  choices: ["splice( )", "unshift( )", "push( )", "pop( )"],
+  answer: "unshift( )"
 },
 {
   question: "Which method creates a new array with the results of calling a provided function on every element in this array.",
-  choices: ["forEach()", "every()", "reduce()", "map()"],
-  answer: "map()"
+  choices: ["forEach( )", "every( )", "reduce( )", "map( )"],
+  answer: "map( )"
 },
 {
   question: "Which method adds and/or removes elements from an array.",
-  choices: ["filter()", "splice()", "pop()", "shift()"],
-  answer: "splice()"
+  choices: ["filter( )", "splice( )", "pop( )", "shift( )"],
+  answer: "splice( )"
 },
 {
   question: "Which method adds one or more elements at the end of an array and returns the new length of the array.",
-  choices: ["shift()", "pop()", "push()", "shift()"],
-  answer: "push()"
+  choices: ["shift( )", "pop( )", "push( )", "shift( )"],
+  answer: "push( )"
 }];
 
 // functions needed to:
@@ -60,19 +60,34 @@ var questionsArray = [{
 // view the highscores?
 
 var body = document.body;
+var h1El = document.createElement('h1');
+h1El.textContent = "JavaScript Fundamentals Quiz!"
+body.appendChild(h1El);
+
+var h3El = document.createElement('h3');
+h3El.textContent = "Click Start to Play!";
+body.appendChild(h3El);
+
+var startButton = document.createElement('button');
+startButton.textContent = "Start!";
+body.appendChild(startButton);
+
+var timerEl = document.getElementById("timer");
+var choicesEl = document.createElement("choice-button");
+
+var quizQuestion = document.createElement('h2');
+var choice1 = document.createElement('button');
+var choice2 = document.createElement('button');
+var choice3 = document.createElement('button');
+var choice4 = document.createElement('button');
+
 var countdown;
 var currentQuestion = -1;
 
 
-var timerEl = document.getElementById("timer");
-var quizBodyEl = document.querySelector("quizBody");
-
-var choicesEl = document.createElement("choice-button");
-
-
 // Timer that counts down from 75
 function startGame() {
-  var timeLeft = 15;
+  var timeLeft = 5;
 
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   countdown = setInterval(function() {
@@ -93,7 +108,6 @@ function startGame() {
   nextQuestion();
 };
 
-var startButton = document.getElementById("start");
 startButton.onclick = startGame;
 
 
@@ -101,9 +115,15 @@ startButton.onclick = startGame;
 function endGame() {
   clearInterval(countdown);
 
-  var h2endGame = document.createElement('h2');
-  h2endGame.textContent = 'Game Over';
-  body.appendChild(h2endGame);
+  var quizBody = 
+  `
+  <h2>Game Over</h2>
+  <h3>put the score here</h3>
+  <input type = "text" id="name" placeholder = "Initials"></input>
+  <button onclick="setScore()">Submit Score</button>
+  `;
+
+  document.getElementById("quizMessage").innerHTML = quizBody;
 }
 
 //loop through questions
@@ -114,30 +134,43 @@ function nextQuestion() {
     endGame();
     return;
   }
+    
+  //removes start message
+  /*h1El.body.removeChild(h1El);
+  h3El.body.removeChild(h3El);
+  startButton.body.removeChild(startButton);*/
+  h1El = body.removeChild(h1El);
+  h3El = body.removeChild(h3El);
+  startButton = body.removeChild(startButton);
 
   //displays the current question
-  var displayedQuestion = document.createElement('div');
-  displayedQuestion.textContent = questionsArray[currentQuestion].question;
-  body.appendChild(displayedQuestion);
+  quizQuestion.textContent = questionsArray[currentQuestion].question;
+  body.appendChild(quizQuestion);
 
-  //loops through the question choices and creates buttons for the choices
-  for (var i = 0; i < questionsArray[currentQuestion].choices.length; i++) {
-    var choiceButton = document.createElement('button');
-    choiceButton.textContent = questionsArray[currentQuestion].choices[i];
-    body.appendChild(choiceButton);
-    
-    answerSelected = choiceButton.setAttribute("onclick", questionsArray[currentQuestion].choices[i]);
-  }
+  //displays the current question's choices
+  choice1.textContent = questionsArray[currentQuestion].choices[0];
+  choices.appendChild(choice1);
 
-}
+  //TODO: I think I need to use setAttribute and set the 'value' so it gets passed to the eventhandler
+  choice2.textContent = questionsArray[currentQuestion].choices[1];
+  choices.appendChild(choice2);
+  choice3.textContent = questionsArray[currentQuestion].choices[2];
+  choices.appendChild(choice3);
+  choice4.textContent = questionsArray[currentQuestion].choices[3];
+  choices.appendChild(choice4);
 
-function isCorrect(answerSelected) {
+  //TODO: add eventhandlers here to compare choice to answer
+
+
+/*
+function isCorrect() {
   // choice correct?
-  if (answerSelected !== questionsArray[currentQuestion].answer) {
+  if (this.value !== questionsArray[currentQuestion].answer) {
     this.timeLeft -= 10;
   } else {
     this.timeLeft +=100;
   }
 }
+*/
 
-quizBodyEl.addEventListener("click", isCorrect);
+//quizBodyEl.addEventListener("click", isCorrect);
