d7a58311d393e84453afff350a44e81e216613b0
Tim Weyel <tweyel@gmail.com>
2021-02-28 17:23:26 -0800

    another fix attempt for github page

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,7 +61,7 @@ var scoresLink = document.createTextNode("Scores");
 a.setAttribute('style', 'text-align: left');
 a.appendChild(scoresLink);
 a.title = 'Scores';
-a.href = "/scores.html";
+a.href = "./scores.html";
 nav.appendChild(a);
 
 scoresLink.addEventListener('click', function(event) {
